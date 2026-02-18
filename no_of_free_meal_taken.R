acfb0449953582c1b816f0d28d5392b41844c373
Mahesh Gorai <maheshgorai202@gmail.com>
2026-02-18 18:55:53 +0530

    Add files via upload

---
+++ no_of_free_meal_taken.R
@@ -0,0 +1,111 @@
+library(dplyr)
+library(data.table)
+library(tidyr)
+library(writexl)
+
+setwd("D:/Sch_edu/HCES")
+
+L2S3 <- fread("D:/Sch_edu/HCES/LEVEL - 02 (Section 3).txt")
+L4T2 <- fread("D:/Sch_edu/HCES/Demographic and other particulars of household members - Block 4  - Level 4 - Type 2 - 68(2011-2012).txt")
+View(L2S3)
+
+free_meal <- L2S3 %>%
+  mutate(
+    Sector = case_when(
+      V4 == 1 ~ "Rural",
+      V4 == 2 ~ "Urban"
+    ),
+    Free_meal_taken = case_when(
+      V29 > 20 ~ "More than 20",
+      V29 <=20 & V29 >= 11 ~ "11 to 10",
+      V29 <=10 & V29 >=5 ~ "5 to 10",
+      V29 <=4 & V29 >=1  ~ "1 to 4",
+      TRUE ~ "not taken"
+    ),
+    final_weight = V36/100,
+    Age_Group = case_when(
+      V22 >= 0 & V22 <= 5 ~ "Age 0-5",
+      V22 >=6 & V22<=15 ~ "Age 6-15",
+      TRUE ~ NA_character_
+    ),
+    State = V5,
+  ) %>%
+  filter(!is.na(Age_Group))
+
+# Compute state-level aggregates
+state_level <- free_meal %>%
+  group_by(State, Age_Group, Sector,Free_meal_taken) %>%
+  summarise(Population = sum(final_weight, na.rm = TRUE), .groups = "drop")%>%
+  mutate(State = as.character(State))
+
+# Compute India-level aggregates (by Sector only)
+india_level <- free_meal %>%
+  group_by(Age_Group, Sector,Free_meal_taken) %>%
+  summarise(Population = sum(final_weight, na.rm = TRUE), .groups = "drop") %>%
+  mutate(State = "India")  
+
+# Combine state and India levels
+free_meal_bind <- bind_rows(state_level, india_level)
+
+# Pivot wider
+combined_wide <- free_meal_bind %>%
+  pivot_wider(
+    names_from = Age_Group,
+    values_from = Population,
+    values_fill = 0
+  ) %>%
+  mutate(State = as.character(State)) %>%  
+  arrange(State)
+
+# Add State_Name
+combined_wide <- combined_wide %>%
+  mutate(State_Name = case_when(
+    State == "India" ~ "India",
+    State == "1" ~ "Jammu & Kashmir",
+    State == "2" ~ "Himachal Pradesh",
+    State == "3" ~ "Punjab",
+    State == "4" ~ "Chandigarh",
+    State == "5" ~ "Uttarakhand",
+    State == "6" ~ "Haryana",
+    State == "7" ~ "Delhi",
+    State == "8" ~ "Rajasthan",
+    State == "9" ~ "Uttar Pradesh",
+    State == "10" ~ "Bihar",
+    State == "11" ~ "Sikkim",
+    State == "12" ~ "Arunachal Pradesh",
+    State == "13" ~ "Nagaland",
+    State == "14" ~ "Manipur",
+    State == "15" ~ "Mizoram",
+    State == "16" ~ "Tripura",
+    State == "17" ~ "Meghalaya",
+    State == "18" ~ "Assam",
+    State == "19" ~ "West Bengal",
+    State == "20" ~ "Jharkhand",
+    State == "21" ~ "Odisha",
+    State == "22" ~ "Chhattisgarh",
+    State == "23" ~ "Madhya Pradesh",
+    State == "24" ~ "Gujarat",
+    State == "25" ~ "Daman & Diu, Dadra & Nagar Haveli",
+    State == "27" ~ "Maharashtra",
+    State == "28" ~ "Andhra Pradesh",
+    State == "29" ~ "Karnataka",
+    State == "30" ~ "Goa",
+    State == "31" ~ "Lakshadweep",
+    State == "32" ~ "Kerala",
+    State == "33" ~ "Tamil Nadu",
+    State == "34" ~ "Puducherry",
+    State == "35" ~ "Andaman & Nicobar Islands",
+    State == "36" ~ "Telangana",
+    State == "37" ~ "Ladakh",
+    TRUE ~ "Unknown"
+  ))
+
+View(combined_wide)
+
+free_meal_all <- combined_wide %>%
+  select(State_Name, Sector,Free_meal_taken, `Age 0-5`, `Age 6-15`)
+
+View(free_meal_all)
+
+write_xlsx(free_meal_all, "No_of_free_Meal_taken(Age 0-5 and 6-15)_2023-24.xlsx")
+
