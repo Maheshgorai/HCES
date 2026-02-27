a6afefaa7ea1e8fb6ed31e67c2272100a3d2ce8a
Mahesh Gorai <maheshgorai202@gmail.com>
2026-02-27 23:55:12 +0530

    Update free_meal.R

--- free_meal.R
+++ free_meal.R
@@ -3,7 +3,7 @@ library(data.table)
 library(tidyr)
 library(writexl)
 
-L2S3 <- fread("D:/Sch_edu/HCES/LEVEL - 02 (Section 3).txt")
+L2S3 <- fread("D:/*****/*****/LEVEL - 02 (Section 3).txt")
 L4T2 <- fread("D:/Sch_edu/HCES/Demographic and other particulars of household members - Block 4  - Level 4 - Type 2 - 68(2011-2012).txt")
 
 
@@ -179,6 +179,7 @@ combined_wide1 <- combined_wide1 %>%
 
 free_meal1_all <- combined_wide1 %>%
   select(State, State_Name, Sector, everything())
+
 
 
 
