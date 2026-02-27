efbdaec5dbdaca293c84dfa2e6d8834a3539f744
Mahesh Gorai <maheshgorai202@gmail.com>
2026-02-27 23:56:36 +0530

    Update Population_Age_wise.R

--- Population_Age_wise.R
+++ Population_Age_wise.R
@@ -114,7 +114,6 @@ free_meal1 <- L4T2 %>%
     State = V36,
   ) %>%
   filter(!is.na(Age_Group))
-View(free_meal1)
 
 state_level1 <- free_meal1 %>%
   group_by(State, Age_Group, Sector) %>%
@@ -181,6 +180,7 @@ combined_wide1 <- combined_wide1 %>%
 
 free_meal1_all <- combined_wide1 %>%
   select(State, State_Name, Sector, everything())
+
 
 
 
